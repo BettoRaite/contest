21211ebfdbbefec57e4572f49fd6736367369ff9
bettoraite <bettoraite@gmail.com>
2024-12-16 18:14:05 +0500

    feat: demo implementation of jwt auth (bearer-token in cookie)

--- module-b/src/server.ts
+++ module-b/src/server.ts
@@ -10,9 +10,13 @@ import { type User, users } from "./models/user.js";
 import { SelectionRange } from "typescript";
 import cookieParser from "cookie-parser";
 import { getJWT } from "./utils.js";
+import { StatusCodes } from "http-status-codes";
+
 const app = express();
 const PORT = 3000;
 const SECRET_KEY = "no_secret_key";
+const SALT = 10;
+const BEARER_TOKEN_NAME = "bearer-token";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +28,7 @@ app.get("", (req, res) => {
   res.redirect("/register");
 });
 app.get("/register", async (req, res) => {
-  res.render("index");
+  res.render("register");
 });
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
@@ -32,24 +36,20 @@ app.post("/register", async (req, res) => {
     res.status(401).json({ message: `Invalid data ${username} ${password}` });
     return;
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, SALT);
   const newUser: User = {
     id: users.length + 1,
     username,
     password: hashedPassword,
   };
   users.push(newUser);
-  const token = getJWT(newUser.id);
-  console.log(newUser);
   res
-    .status(200)
-    .cookie("token", token, {
-      maxAge: 24 * 60 * 60 * 1000,
-    })
-    .json({
-      message: "Signup successful",
-      token,
-    });
+    .status(StatusCodes.OK)
+    .json({ message: "User registered successfully.", nextUrl: "/login" });
+});
+
+app.get("/login", async (req, res) => {
+  res.render("login");
 });
 
 app.post("/login", async (req, res) => {
@@ -60,36 +60,44 @@ app.post("/login", async (req, res) => {
     res.status(401).json({ message: "Invalid credentials" });
     return;
   }
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
+  const arePasswordsMatching = await bcrypt.compare(password, user.password);
+  if (!arePasswordsMatching) {
     res.status(401).json({ message: "Invalid credentials" });
     return;
   }
 
-  const token = jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, {
+  const token = jwt.sign({ id: user.id }, SECRET_KEY, {
     expiresIn: "1h",
   });
-  res.json({ token });
+
+  res
+    .status(StatusCodes.OK)
+    .cookie(BEARER_TOKEN_NAME, token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 3600000,
+    })
+    .json({ message: "User registered successfully.", nextUrl: "/profile" });
 });
 
 // Middleware to protect routes
-const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-  let token: string;
-  if (req.headers.authorization?.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
-  } else {
-    res.status(401).json({ message: "User not authorized" });
-    return;
-  }
+const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const token: string = req.cookies[BEARER_TOKEN_NAME];
   const { id } = jwt.verify(token, SECRET_KEY) as { id: number };
-  req.userId = users[id];
+  if (!Number.isFinite(id)) {
+    res.status(401).json({ message: "User not authorized" });
+  }
+  req.body.user = users.find((u) => {
+    return u.id === id;
+  });
   next();
 };
 
 // Protected route example
-app.get("/protected", authenticateJWT, (req: Request, res: Response) => {
-  const user = users[req.userId];
-  res.json({ message: "This is a protected route", user });
+app.get("/profile", authenticateToken, (req: Request, res: Response) => {
+  const user = req.body.user;
+  console.log(req.body);
+  res.render("profile", { username: user.username });
 });
 
 app.listen(PORT, () => {
